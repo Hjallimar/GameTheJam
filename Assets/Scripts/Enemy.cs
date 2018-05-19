48b5f59c93ee5d6c40cb7808bd686d4e4008c69f
Cabamacadaf <maccus9@hotmail.com>
2018-05-19 19:36:47 +0200

    la till en fiende

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+    public float speed = 10f;
+    public float timeUntilJump = 1f;
+    public float jumpForce = 50f;
+
+    private Rigidbody2D rb2d;
+    public Transform frontCheck;
+    public Transform groundCheck;
+    private bool grounded = false;
+    private float jumpTimer = 0f;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(-speed * Time.deltaTime, 0, 0);
+        grounded = Physics2D.OverlapPoint(groundCheck.position);
+        //rb2d.AddForce(new Vector2(-speed, 0));
+        if (Physics2D.OverlapPoint(frontCheck.position) && !Physics2D.OverlapPoint(frontCheck.position).CompareTag("Player")) {
+            Flip();
+        }
+        if(jumpTimer >= timeUntilJump && grounded) {
+            Jump();
+            jumpTimer = 0;
+        }
+        jumpTimer += Time.deltaTime;
+    }
+
+    private void Flip () {
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+        speed = -speed;
+    }
+
+    private void Jump () {
+        rb2d.AddForce(new Vector2(0, jumpForce));
+    }
+
+    private void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.collider.CompareTag("Player")) {
+            //Destroy(collision.gameObject);
+        }
+    }
+}
