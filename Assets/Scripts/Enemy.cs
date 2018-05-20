099cf1418e109f10cbaf288102421e67940170ee
Hjallimar <hjalle@ownit.nu>
2018-05-20 16:37:09 +0200

    we have a game

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -51,6 +51,7 @@ public class Enemy : MonoBehaviour {
     private void OnCollisionEnter2D (Collision2D collision) {
         if (collision.collider.CompareTag("Player")) {
             collision.collider.GetComponent<PlayerVariables>().Respawn();
+            Destroy(this.gameObject);
         }
     }
 }
