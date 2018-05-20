dec9cb013210fc582bd77495f105e88276e5a2d2
Cabamacadaf <maccus9@hotmail.com>
2018-05-20 15:07:28 +0200

    saker

--- Assets/Scripts/ExplodingEnemy.cs
+++ Assets/Scripts/ExplodingEnemy.cs
@@ -6,6 +6,7 @@ public class ExplodingEnemy : MonoBehaviour {
 
     public float speed = 7f;
 
+    private bool facingRight = false;
     private Animator anim;
     private Rigidbody2D rb2d;
     public Transform player;
@@ -24,12 +25,23 @@ public class ExplodingEnemy : MonoBehaviour {
         if (playerIsNear) {
             anim.SetBool("Aggro", true);
             transform.position = Vector3.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
+            if (facingRight && player.position.x - transform.position.x > 0) {
+                FlipLeft();
+            }
+            if(!facingRight && player.position.x - transform.position.x < 0) {
+                FlipRight();
+            }
         }
     }
 
-    private void Flip () {
+    private void FlipLeft () {
+        facingRight = false;
         transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-        speed = -speed;
+    }
+
+    private void FlipRight () {
+        facingRight = true;
+        transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
     }
 
     private void OnCollisionEnter2D (Collision2D collision) {
