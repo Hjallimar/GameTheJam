0b907a0fb8dd8a248259df927e6737c71ea2d2fb
Hjallimar <hjalle@ownit.nu>
2018-05-20 13:49:27 +0200

    sort of functioning lvl 3

---
+++ Assets/Scripts/CakePowerUp.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CakePowerUp : MonoBehaviour {
+
+    public int life = 1;
+    public GameObject player;
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerVariables>().life += 1;
+        }
+    }
+}
